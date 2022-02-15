7cd89fe772550af9051e5b7f7e8b4440be43e408
Kadir Niyazi AÇAR <kadirnacar@kadirnacar.com>
2022-02-15 18:52:30 +0300

    .

---
+++ packages/viewer/src/app/withRouter.tsx
@@ -0,0 +1,33 @@
+import {
+  Location,
+  NavigateFunction,
+  Params,
+  useLocation,
+  useNavigate,
+  useParams,
+  
+} from 'react-router-dom';
+import * as React from 'react';
+
+export interface WithRouter {
+  navigate?: NavigateFunction;
+  location?: Location;
+  params?: Readonly<Params<string>>;
+}
+export const withRouter = (Component) => {
+  const Wrapper = (props) => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const params = useParams();
+    return (
+      <Component
+        navigate={navigate}
+        params={params}
+        location={location}
+        {...props}
+      />
+    );
+  };
+
+  return Wrapper;
+};
